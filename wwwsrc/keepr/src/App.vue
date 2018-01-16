e04a122f15c7e000ac85f424df3a0bd78095a88f
Bryce Murray <brycepmurray711@gmail.com>
2018-01-16 11:20:34 -0700

    error with not finding the component...cant find Navbar...

--- wwwsrc/keepr/src/App.vue
+++ wwwsrc/keepr/src/App.vue
@@ -1,23 +1,46 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
-</template>
+    <div id="no" class="container-fluid">
+      <div id="app">
+        <navbar></navbar>
+        
+        <router-view></router-view>
+      </div>
+    </div>
+  </template>
 
 <script>
-export default {
-  name: 'App'
-}
+    import Error from './components/Error'
+    import Navbar from './components/Navbar'
+    export default {
+        name: 'app',
+        components: {
+            Error,
+            Navbar,
+        },
+        methods: {},
+        mounted() {
+            this.$store.dispatch('authenticate')
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: 'Montserrat', cursive;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        background-image: url('http://www.myfreetextures.com/wp-content/uploads/2014/10/seamless-wood-texture-2.jpg');
+        min-height: 100vh;
+    }
+    
+    #no {
+        padding-left: 0px;
+        padding-right: 0px;
+    }
+    
+    button {
+        float: right
+    }
 </style>
---
+++ wwwsrc/keepr/src/components/Navbar.vue
@@ -0,0 +1,129 @@
+<div>
+    <ul>
+        <div class="col-xs-5">
+            <li>
+                <a class="home" href="#">Home</a>
+            </li>
+            <li>
+                <a href="#cookbook">Cookbook</a>
+            </li>
+            <li>
+                <a href="#shopping">Shopping List</a>
+            </li>
+
+
+
+        </div>
+        <div class="col-xs-4 text-left">
+
+            <li>
+                <h3 v-if="user.name">Welcome, {{user.name}}</h3>
+                <h3 v-if="!user.name">Welcome, traveler</h3>
+            </li>
+        </div>
+
+        <li v-if="user.name" class="logout pull-right">
+            <a v-if="user.name" @click="logout">Log Out</a>
+        </li>
+        <li v-if="!user.name" class="logout">
+            <a href="#login">Log In</a>
+        </li>
+    </ul>
+
+</div>
+</template>
+
+
+
+<script>
+    const swal = require('sweetalert2')
+    export default {
+        name: 'navbar',
+        data() {
+            return {}
+        },
+        computed: {
+            user() {
+                return this.$store.state.user
+            }
+        },
+        methods: {
+            logout() {
+                swal({
+                    title: 'Are you sure?',
+                    type: 'warning',
+                    showCancelButton: true,
+                    cancelButtonColor: '#d33',
+                    confirmButtonColor: '#3085d6',
+                    cancelButtonText: "No, I'm not sure",
+                    confirmButtonText: "Yes, I'm sure",
+                    cancelButtonClass: 'btn btn-danger',
+                    confirmButtonClass: 'btn btn-success',
+                    buttonsStyling: false,
+                    reverseButtons: true
+                }).then((result) => {
+                    if (result.value) {
+                        swal(
+                            "",
+                            "You've been logged out.",
+                            'success'
+                        )
+                        this.$store.dispatch('logout')
+                    } else if (result.dismiss === 'cancel') {
+                        return
+                    }
+                })
+            },
+            login() {
+                this.$store.dispatch('authenticate')
+            }
+        },
+        components: {}
+    }
+</script>
+
+
+<style scoped>
+    ul {
+        list-style-type: none;
+        display: flex;
+        justify-content: space-between;
+        margin: 0;
+        padding: 0;
+        overflow: hidden;
+        background-color: #333;
+    }
+    
+    li {
+        float: left;
+    }
+    
+    li a {
+        display: block;
+        color: white;
+        text-align: center;
+        padding: 14px 16px;
+        font-family: 'Montserrat', cursive;
+    }
+    
+    h3 {
+        color: white;
+        display: block;
+        text-align: center;
+        font-family: 'Montserrat', cursive;
+        margin-top: 9px
+    }
+    
+    .logout {
+        right: 0px;
+        cursor: pointer;
+    }
+    
+    .logout:hover {
+        color: red;
+    }
+    
+    li a:hover {
+        background-color: #111;
+    }
+</style>
